77ba3c57a8e20eff5b342c94cfb1f3e3004124aa
swiftleah <leahrosehenry@gmail.com>
2023-11-20 11:12:17 -0800

    Added more tests to test_city.py & updated old tests that did not work anymore

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """ """
+import unittest
+from sqlalchemy import create_engine, String
+from sqlalchemy.orm import sessionmaker, class_mapper
+from models.base_model import Base
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class test_City(test_basemodel):
-    """ """
+class test_City(unittest.TestCase):
+    """ test cases for city.py """
+
+    def test_table_name(self):
+        """ test to see if __tablename__ is 'cities' """
+        self.assertEqual(City.__tablename__, 'cities')
+
+    @classmethod
+    def teardownclass(cls):
+        """ tests that session closes and all tables are dropped """
+        cls.session.close_all()
+        Base.metadata.drop_all(cls.engine)
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -14,11 +28,21 @@ class test_City(test_basemodel):
         self.value = City
 
     def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        """ testing that state_id is of type String """
+        state_id = class_mapper(City).mapped_table.columns['state_id']
+        self.assertIsInstance(state_id.type, String)
 
     def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        """ tests that name is of type String """
+        name = class_mapper(City).mapped_table.columns['name']
+        self.assertIsInstance(name.type, String)
+
+    def test_attributes(self):
+        """ tests attributes for city """
+        city = City(state_id='state_id_1', name='Canada')
+        self.assertEqual(city.state_id, 'state_id_1')
+        self.assertEqual(city.name, 'Canada')
+
+
+if __name__ == '__main__':
+    unittest.main()
