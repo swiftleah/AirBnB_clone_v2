5972bfa6e5d7ba71c23a5027a0b738d7283f5018
swiftleah <leahrosehenry@gmail.com>
2023-11-22 07:54:58 -0800

    Updated city.py file

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
@@ -7,6 +8,10 @@ from sqlalchemy import Column, String, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name. 
     inherits from Base """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
