bac202bc6a9820bc02f97db08d6cecc0a34f5c7c
swiftleah <leahrosehenry@gmail.com>
2023-11-22 10:26:06 -0800

    Made __init__ shorter in dbstorage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -24,14 +24,11 @@ class DBStorage:
 
     def __init__(self):
         """Instantiates the DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB))
+        self.__engine = create_engine(
+                f"mysql+mysqldb://{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}@{getenv('HBNB_MYSQL_HOST')}/{getenv('HBNB_MYSQL_DB')}"
+                )
 
-        if HBNB_ENV == "test":
+        if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
