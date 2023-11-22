585ff94ad7a0f9a5d63b784aee4177bf035781d9
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-22 18:07:34 +0100

    -

--- models/state.py
+++ models/state.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" defines the class State"""
+"""Defines the State class."""
+
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class State(BaseModel, Base):
-    """Representation of state """
+    """Representation of a state."""
     if models.switch == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
@@ -19,16 +17,12 @@ class State(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes state"""
+        """Initializes a state instance."""
         super().__init__(*args, **kwargs)
 
     if models.switch != "db":
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """Getter for a list of city instances related to the state."""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
