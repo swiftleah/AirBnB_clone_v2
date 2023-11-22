99d2e48671695d9401eb1905226d282f7be6466d
Sliman Elmansouri <elmansourisliman@gmail.com>
2023-11-22 18:01:15 +0100

    -

--- models/city.py
+++ models/city.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
-""" defines the class City"""
+"""Defines the City class."""
+
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representation of a city."""
     if models.switch == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
@@ -20,5 +18,5 @@ class City(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """Initializes a city instance."""
         super().__init__(*args, **kwargs)
