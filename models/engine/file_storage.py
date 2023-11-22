7db139d34892bb95d3fcc4d9eb9464d06ef02561
swiftleah <leahrosehenry@gmail.com>
2023-11-22 10:18:56 -0800

    cleaned up documentation for file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
-
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -10,35 +9,36 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """ Serializes instances -> JSON file -> deserializes -> instances """
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """ Returns dictionary __objects """
         if cls:
             return {key: value for key, value in self.__objects.items() if cls == value.__class__ or cls == value.__class__.__name__}
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """ Sets in __objects the obj with key <obj class name>.id """
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """ Serializes __objects to JSON file ( __file_path"""
         json_objects = {key: value.to_dict() for key, value in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
              json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """ Deserializes JSON file to __objects """
         try:
             with open(self.__file_path, 'r') as file:
                 jsn = json.load(file)
