bdff9a79177ed3b3a7fe20a14deaea934c1f61db
swiftleah <leahrosehenry@gmail.com>
2023-12-30 14:35:32 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" script starts a Flask web application listening on 0.0.0.0
+	port 5000, Route '/' displaying 'Hello HBNB!' """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	""" message that says 'Hello HBNB!' """
+	return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ prints message """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def only_c(text):
+    """ only_c method: route to return C followed by text variable, replaces _
+        with spaces """
+    text = text.replace('_', ' ')
+    return ('C' + ' ' + text)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<path:text>', strict_slashes=False)
+def only_python(text=None):
+    """ only_python method: route to return text follow by "is cool"
+        (can be overwritten), replaces _ with spaces """
+    if text is None:
+        text = 'is cool'
+    else:
+        text = text.replace('_', ' ')
+    return ('Python' + ' ' + text)
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
