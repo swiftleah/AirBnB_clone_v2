59210ed14d0ccbad982460c4fd85d3e8ec0d8514
swiftleah <leahrosehenry@gmail.com>
2023-12-30 14:24:20 +0000

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,13 @@
 """ script starts a Flask web application listening on 0.0.0.0
 	port 5000, Route '/' displaying 'Hello HBNB!' """
 from flask import Flask
-
-
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
 	""" message that says 'Hello HBNB!' """
-	return ("Hello HBNB!")
+	return "Hello HBNB!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
